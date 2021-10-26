604978e9595b86be44a68c967a620669c7d6968e
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2021-10-26 20:49:39 +0100

    Implemented Level Tile Functionality
    Added take_damage function and health variable

--- scripts/LevelTile.gd
+++ scripts/LevelTile.gd
@@ -1,14 +1,20 @@
 extends StaticBody2D
 
-
 # Member Variables
-
+export var health = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_to_group("LevelTiles")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# func _process(delta):
+	# pass
+
+
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0:
+		queue_free()
