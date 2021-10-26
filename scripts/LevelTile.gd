2ff90e02c4f4761e0fd48659e73562c07ec2f6d5
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2021-10-26 20:12:48 +0100

    Worked a Little on Level Tile
    Added to LevelTiles Group, as well as setting up the collision layers.

--- scripts/LevelTile.gd
+++ scripts/LevelTile.gd
@@ -1,14 +1,12 @@
 extends StaticBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Member Variables
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_to_group("LevelTiles")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
