600354f0595efab99429034381e1c65bf5c0477d
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2021-10-26 20:07:20 +0100

    Created Basic Enemy
    Implemented basic falling Functionality

---
+++ scripts/FallingEnemy.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+# Member Variables
+export var damage = 1
+export var health = 5
+
+var velocity = Vector2(0.0, 2.0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_to_group("Enemies")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
+	var collision_data = move_and_collide(velocity)
